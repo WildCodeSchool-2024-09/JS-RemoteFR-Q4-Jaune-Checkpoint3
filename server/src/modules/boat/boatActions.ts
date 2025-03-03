c14386bfb7752be932eaf1e1a4e9f71227a0eb48
Lisa BELLALI <lisa.bellali92@gmail.com>
2025-03-03 12:26:55 +0100

    finished step 3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,23 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boatId = {
+      id: Number(req.params.id),
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    const affectedRows = await boatRepository.update(boatId);
+
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -4,15 +4,15 @@ import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-     // Fetch all tiles from the database
-     const tiles = await tileRepository.readAll();
- 
-     // Respond with the tiles in JSON format
-     res.json(tiles);
-   } catch (err) {
-     // Pass any errors to the error-handling middleware
-     next(err);
-   }
+    // Fetch all tiles from the database
+    const tiles = await tileRepository.readAll();
+
+    // Respond with the tiles in JSON format
+    res.json(tiles);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {

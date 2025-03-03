0090d54da83f58d981f42653372c82a47fc5ad6a
maxime <trousselmaxime@gmail.com>
2025-03-03 10:48:55 +0100

    step2

--- server/src/router.ts
+++ server/src/router.ts
@@ -11,6 +11,9 @@ import boatActions from "./modules/boat/boatActions";
 router.get("/api/boats", boatActions.browse);
 
 import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
+
+router.get("/api/tiles", tileActions.browse);
 
 router.post("/api/games", gameActions.add);
 

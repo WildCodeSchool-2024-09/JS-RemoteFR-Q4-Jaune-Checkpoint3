ee98737db968cbfc8b854cc36a30e77d0fa822fd
Killian0111 <45.killian.georges@gmail.com>
2025-03-03 10:34:37 +0100

    finished step 3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -17,6 +17,21 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const edit: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    const boatToUpdate = {
+      coord_x: Number(req.body.coord_x),
+      coord_y: Number(req.body.coord_y),
+      id: Number(req.params.id),
+    };
+    const affectedRows = await boatRepository.update(boatToUpdate);
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {

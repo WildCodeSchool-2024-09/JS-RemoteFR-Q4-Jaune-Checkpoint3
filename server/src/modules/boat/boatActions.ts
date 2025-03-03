2fe7ec9fb6edbbc64fd3e923da2983ea7f4b89e9
Jon33260 <j.mallet82@laposte.net>
2025-03-03 10:26:13 +0100

    step3_finish

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -4,19 +4,33 @@ import boatRepository from "./boatRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const boats = await boatRepository.readAll();
-
-    // Respond with the boats in JSON format
     res.json(boats);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    // Update a specific category based on the provided ID
+
+    const boat = {
+      id: Number(req.params.id),
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    const affectedRows = await boatRepository.update(boat);
+
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {

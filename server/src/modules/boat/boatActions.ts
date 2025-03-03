9516e692f9c721e22fab7108c4f83e8b795d2a75
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-03-03 10:32:32 +0100

    finished step3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -4,22 +4,50 @@ import boatRepository from "./boatRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const boats = await boatRepository.readAll();
 
-    // Respond with the boats in JSON format
     res.json(boats);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+const read: RequestHandler = async (req, res, next) => {
+  try {
+    const boatId = Number(req.params.id);
+    const boats = await boatRepository.read(boatId);
+    if (boats === null) {
+      res.sendStatus(404);
+    } else {
+      res.json(boats);
+    }
+  } catch (err) {
     next(err);
   }
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boats = {
+      id: Number(req.params.id),
+      name: req.body.name,
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+    const affectedRows = await boatRepository.update(boats);
+
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
   browse,
+  read,
   edit,
 };
--- server/src/router.ts
+++ server/src/router.ts
@@ -9,6 +9,8 @@ const router = express.Router();
 import boatActions from "./modules/boat/boatActions";
 
 router.get("/api/boats", boatActions.browse);
+router.get("/api/boats/:id", boatActions.read);
+router.put("/api/boats/:id", boatActions.edit);
 
 import gameActions from "./modules/game/gameActions";
 

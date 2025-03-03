3a7bc75510d6a620be1521b3fca2102dae6c5b4e
MaxenceGitDev <maxence.pruvot@icloud.com>
2025-03-03 10:19:03 +0100

    step 2 done

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,14 +1,22 @@
 import type { RequestHandler } from "express";
 
+import tileRepository from "./tileRepository";
+
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+	try {
+		const tiles = await tileRepository.readAll();
+
+		res.json(tiles);
+	} catch (err) {
+		next(err);
+	}
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+	// your code here
 };
 
 export default {
-  browse,
-  validate,
+	browse,
+	validate,
 };
